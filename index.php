8649b8d3296309be7550fe61aa40f0886f67b023
Luis Enrique <luisenriquehuhpuc@hotmail.com>
2015-10-25 22:14:56 -0500

    Pagina Principal
    He creado la pagina principal, aun no a quedado del todo, pero es una
    vista para el publico, en la barra de navegación tendrá las opciones de
    iniciar sesion y registrarse

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Portal de Empleo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<?php include("head-footer/navbar-principal.php"); ?>
+	</header>
+	<main>
+		
+	</main>
+	<footer>
+		
+	</footer>
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+</body>
+</html>
